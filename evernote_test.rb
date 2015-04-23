b45aab0220b9f7c2ca59b93a725a70276cf08e98
Colin <colinwmackenzie@gmail.com>
2015-04-22 23:21:23 -0400

    eod

--- evernote_test.rb
+++ evernote_test.rb
@@ -1,1 +1,5 @@
-require "evernote_auth.rb"
+require_relative 'evernote_config.rb'
+
+client = EvernoteOAuth::Client.new(consumer_key:OAUTH_CONSUMER_KEY, consumer_secret:OAUTH_CONSUMER_SECRET, sandbox: false)
+
+puts client
